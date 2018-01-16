1b7f6694a7cc18b1b415ec9cfc75f30348abdf44
Daniela Meneses <dvargas@oildex.com>
2018-01-16 08:10:58 -0400

    Added linked list code and initial eclipse config

---
+++ src/LinkedListUtils.java
@@ -0,0 +1,102 @@
+
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+/*
+ * SD2x Homework #1
+ * Implement the methods below according to the specification in the assignment description.
+ * Please be sure not to change the signature of any of the methods!
+ */
+
+public class LinkedListUtils {
+	
+	public static void insertSorted(LinkedList<Integer> list, int value) {
+		if (list == null) {
+			return;
+		}
+
+		if (list.isEmpty()) {
+			list.add(value);
+			return;
+		}
+		
+		ListIterator<Integer> iterator = list.listIterator();
+		int indexToAdd = 0;
+		while(iterator.hasNext()) {
+			int currentNumber = iterator.next();
+			if (value > currentNumber) {
+				indexToAdd = iterator.nextIndex();
+			} else {
+				break;
+			}
+		}
+		
+		list.add(indexToAdd, value);
+	}
+	
+
+	public static void removeMaximumValues(LinkedList<String> list, int N) {
+		for (int index = 0; index < N; index++) {
+			int maxValueIndex = LinkedListUtils.getMaxValueIndex(list);
+			
+			if (maxValueIndex >= 0) {
+				String currentMaxValue = list.get(maxValueIndex);
+				boolean removedMaxValue;
+				do {
+					removedMaxValue = list.remove(currentMaxValue);
+				} while(removedMaxValue);
+			}
+		}
+	}
+	
+	public static int getMaxValueIndex(LinkedList<String> list) {
+		if (list == null) {
+			return -1;
+		}
+		if (list.isEmpty()) {
+			return -1;
+		}
+		int maxValueIndex = 0;
+		String maxValue = list.get(0);
+		for (int i = 1; i < list.size(); i++) {
+			String value = list.get(i);
+			
+			if(value.compareTo(maxValue) > 0) {
+				maxValueIndex = i;
+				maxValue = value;
+				i++;
+			}
+		}
+		return maxValueIndex;
+	}
+	
+	public static boolean containsSubsequence(LinkedList<Integer> one, LinkedList<Integer> two) {
+
+		if (one == null || two == null) {
+			return false;
+		}
+		
+		Iterator<Integer> listIterator = one.iterator();
+		Iterator<Integer> sequenceIterator = two.iterator();
+		boolean match = false;
+		while (listIterator.hasNext()) {
+			Integer listInteger = (Integer) listIterator.next();
+			while (sequenceIterator.hasNext()) {
+				Integer sequenceInteger = (Integer) sequenceIterator.next();
+				if (listInteger == sequenceInteger && listIterator.hasNext()) {
+					listInteger = listIterator.next();
+					match = true;
+				} else {
+					if (match) {
+						sequenceIterator = two.iterator();	
+					}
+					match = false;
+				}
+			}
+		}
+		return match;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*LinkedList<Integer> list = new LinkedList<>();
+
+		LinkedListUtils.insertSorted(list , 2);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 3);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 4);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 8);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 5);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 10);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 7);
+		System.out.println(list);
+		LinkedListUtils.insertSorted(list , 1);
+		System.out.println(list);*/
+		
+		/*LinkedList<String> list = new LinkedList<>();
+		list.add("hola");
+		list.add("hello");
+		list.add("123456");
+		list.add("a");
+		list.add("b");
+		list.add("z");
+		list.add("hola");
+		LinkedListUtils.removeMaximumValues(list, 3);
+		System.out.println(list);*/
+		
+	}
+
+}
